13e3a47ac745bf527169f652e4bd910ecdc4f928
yumiarakawa <28menezesy@pasb.com.br>
2025-08-30 16:15:03 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+
+    while (1 + 1 == 2){
+        printf("===============================\n   Calculadora Simples\n===============================\n");
+        printf("Selecione uma operacao:\n1. Adicao\n2. Subtracao\n3. Multiplicacao\n4. Divisao\n5. Sair\nOpcao: ");
+
+        int A;
+        scanf("%d", &A);
+
+        if (A == 5){
+            break;
+        }
+
+        printf("Digite o primeiro numero: ");
+        int B;
+        scanf("%d", &B);
+
+        printf("Digite o segundo numero: ");
+        int C;
+        scanf("%d", &C);
+
+        if (A == 1){
+            printf("Resultado: %d + %d = %d \n", B, C, B+C);
+        } else if (A == 2){
+            printf("Resultado: %d - %d = %d \n", B, C, B-C);
+        } else if (A == 3){
+            printf("Resultado: %d * %d = %d \n", B, C, B*C);
+        } else if (A == 4){
+            if(C != 0){
+                printf("Resultado: %d / %d = %d \n", B, C, B/C);
+            } else {
+                printf("Resposta invalida. Por favor, digite 's' para sim ou 'n' para nao.\n");
+            }
+        }
+        
+        char simounao;
+        
+        printf("Deseja realizar outra operacao? (s/n): ");
+        scanf(" %c", &simounao);
+        if (simounao == 'n'){
+            break;
+        }
+    }
+
+    printf("Obrigado por usar a calculadora! Ate a proxima.");
+}
